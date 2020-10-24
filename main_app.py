420822408998faa37b635d67c7617d02bcd2eed1
H. Juan David Romero <hermes.romero@unipamplona.edu.co>
2020-10-24 09:20:09 -0500

    Add application files

---
+++ main_app.py
@@ -0,0 +1,51 @@
+import app1
+import app2
+import streamlit as st
+from PIL import Image
+
+# Set the sidebar color
+st.markdown(
+    """
+<style>
+.sidebar .sidebar-content {
+    background-image: linear-gradient(#ED1B24, #ED1B24);
+    color: white;
+}
+</style>
+""",
+    unsafe_allow_html=True,
+)
+
+# Set the sidebar image
+image = Image.open("C:/Users/volun/DS4A/project/assets/CI_logo.png")
+st.sidebar.image(image, use_column_width=True)
+
+# Set the sidebar title
+st.sidebar.title('Ceramica Italia web app')
+
+# Set the sidebar selectbox for choosing dashboard
+PAGES = {
+    "Customer Segmentation": app1,
+    "Google Analytics": app2
+}
+st.sidebar.subheader("Select a dashboard page")
+selection = st.sidebar.selectbox("", list(PAGES.keys()))
+page = PAGES[selection]
+page.app()
+
+# Set author names on the sidebar using markdown
+st.sidebar.markdown(
+
+    """
+AUTHORS:
+
+NATALÍ VELASQUEZ\n
+DANIEL ESCAMILLA\n
+DANNIN GOMEZ\n
+DAVID NAVARRO\n
+FREDY MUÑOZ\n
+HERMES ROMERO\n
+WILSON RODRÍGUEZ\n
+
+
+""")
